854dba35da4fa14fabaf238073344ac860f10629
Joe Scott <jbs198585@gmail.com>
2022-02-10 07:03:45 -0500

    toDo project experimenting

--- todo.js
+++ todo.js
@@ -1,3 +1,12 @@
-const ToDoList = [];
+const toDoList = [];
 
-let newToDo = prompt("What would you like to do?");
+let toDo = prompt("What would you like to do?");
+
+while (toDo == "") {
+  if (toDo === "quit") break;
+  if (toDo === "new") {
+    toDo = prompt("Enter new todo");
+  } else if (toDo === "list") {
+  } else if (toDo === "delete") {
+  }
+}
