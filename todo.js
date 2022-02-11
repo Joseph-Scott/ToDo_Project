1147518a5ad97259eba259fcf3f88c52723c49a6
Joe Scott <jbs198585@gmail.com>
2022-02-11 06:44:02 -0500

    todo list attempt

--- todo.js
+++ todo.js
@@ -4,9 +4,13 @@ let toDo = prompt("What would you like to do?");
 
 while (toDo == "") {
   if (toDo === "quit") break;
-  if (toDo === "new") {
-    toDo = prompt("Enter new todo");
-  } else if (toDo === "list") {
-  } else if (toDo === "delete") {
-  }
+}
+if (toDo === "new") {
+  toDo = prompt("Enter new todo");
+  toDoList.push(toDo);
+} else if (toDo === "list") {
+  console.log(toDoList);
+} else if (toDo === "delete") {
+  toDo = prompt("which todo would you like to delete?");
+  toDo = toDoList.splice(toDo);
 }
