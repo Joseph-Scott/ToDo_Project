473509aba8556a0722baa7f35a9c00107db66846
Joe Scott <jbs198585@gmail.com>
2022-02-12 11:27:58 -0500

    My own attempt

--- todo.js
+++ todo.js
@@ -1,16 +1,18 @@
-const toDoList = [];
+/* This was my attempt at the toDo List on my own*/
+// const toDoList = [];
 
-let toDo = prompt("What would you like to do?");
+// let toDo = prompt("What would you like to do?");
 
-while (toDo == "") {
-  if (toDo === "quit") break;
-}
-if (toDo === "new") {
-  toDo = prompt("Enter new todo");
-  toDoList.push(toDo);
-} else if (toDo === "list") {
-  console.log(toDoList);
-} else if (toDo === "delete") {
-  toDo = prompt("which todo would you like to delete?");
-  toDo = toDoList.splice(toDo);
-}
+// while (toDo !== "quit") {
+//   todo = prompt("What would you like to do?");
+//   if (toDo === "new") {
+//     toDo = prompt("Enter new todo");
+//     toDoList.push(toDo);
+//   } else if (toDo === "list") {
+//     console.log(toDoList);
+//   } else if (toDo === "delete") {
+//     toDo = prompt("which todo would you like to delete?");
+//     toDo = toDoList.splice(toDo);
+//   }
+// }
+// console.log("OK, QUIT THE APP!");
